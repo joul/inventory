d4324bf210cdbe8a22411faa4c5458c23890311c
Jonathan Zulu <joul84@gmail.com>
2023-04-23 16:15:56 +0200

    changes to about and contact page

---
+++ resources/views/contact.blade.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Contact us</title>
+</head>
+<body>
+    <h1>This is test contactus page from controller, all good???</h1>
+    <a href="{{ route('about.page') }}">About using name route</a><br>
+    <a href="{{ url('/about') }}">About page using url</a>
+</body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Demo\DemoController;
 
 /*
 |--------------------------------------------------------------------------
@@ -16,3 +17,17 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+//Update precedures applied in laravel 9 below
+Route::controller(DemoController::class)->group(function(){
+    Route::get('/about', 'Index')->name('about.page')->middleware('check');
+    Route::get('/contact', 'ContactMethod')->name('contact.page'); //url,method name
+});
+
+// //below steps apply in laravel 8
+// Route::get('/about', [DemoController::class, 'Index']);
+// Route::get('/contact', [DemoController::class, 'ContactMethod']);
+
+// Route::get('/about', function () {
+//     return view('about');
+// });
